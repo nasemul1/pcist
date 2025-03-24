3b09e3c423ccb8a670b6f9fb1795af979a6e0fd8
nasemul.me <nasemul.me@gmail.com>
2025-03-24 04:54:13 +0000

    added footer and change app js

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full flex justify-center py-15 bg-slate-950 text-white'>
+        <div className='w-full lg:w-[85%] flex justify-between'>
+            <div>All rights reserved. Copyright © 2025 Programming Club of IST.</div>
+            <div></div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
